8bfd9bb367a30c95fd777429b50b400d375295b4
shuya-tamaru <tamaru1013@gmail.com>
2020-01-24 20:08:27 +0900

    ユーザーモデルにバリデーション記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_year, :birthday_month, :birthday_date ,presence: true
+
 end
