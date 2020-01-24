b43aac006ea183d7354f00100aea20f9919009fc
shuya-tamaru <tamaru1013@gmail.com>
2020-01-24 19:09:21 +0900

    edit-migrationfile_user-model

--- db/migrate/20200124094648_devise_create_users.rb
+++ db/migrate/20200124094648_devise_create_users.rb
@@ -4,6 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, index: true
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.integer :birthday_year,        null: false
+      t.integer :birthday_month,       null: false
+      t.integer :birthday_date,        null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
