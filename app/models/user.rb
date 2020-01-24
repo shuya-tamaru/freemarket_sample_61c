6c2ec3a867fe8a72e421b065452cdc9d913eca2d
shuya-tamaru <tamaru1013@gmail.com>
2020-01-25 00:30:35 +0900

    userモデルとcellphoneモデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_year, :birthday_month, :birthday_date ,presence: true
-
+  has_one :cellphone
 end
