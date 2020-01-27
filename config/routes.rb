f3f518d99311aa47a29e8db8b5fdd5622133ab86
shuya-tamaru <tamaru1013@gmail.com>
2020-01-27 21:25:34 +0900

    アドレス登録画面newからのcreateへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'cellphones', to: 'users/registrations#new_cellphone'
     post 'cellphones', to: 'users/registrations#create_cellphone'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
   end
   root to: "items#index"
   resources :items
