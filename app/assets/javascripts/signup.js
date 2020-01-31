b32e1d7cc944cf995675b3193fb5279530b352cd
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-02-01 03:14:54 +0900

    パスワード表示機能実装(途中)
    ・パスワード欄ではマスキング。
    ・チェックボックスクリックで下に表示されるように実装。
    ただ、問題点はまだあるので、次のコミットで解決する。
    ・クリックしてから入力しないと下に表示されない。
    ・チェックをはずしても表示が残る。
    ・パスワード表示により、下にレイアウトがずれる形になるが、重なってるところがある。

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -2,11 +2,59 @@ $(function() {
   var password  = '#js-password';
   var passcheck = '#js-passcheck';
 
+  changeInputtype(password, passcheck);
+});
+
+function changeInputtype(password, passcheck) {
   $(passcheck).change(function() {
       if ($(this).prop('checked')) {
-          $(password).attr('type','text');
+        $(function(){
+          $(password).on('input', function(){
+            var input_text = $(this).val();
+            $('.contents3__entry__passwordResult').text(input_text);
+          });
+        });
       } else {
-          $(password).attr('type','password');
+        $(function(){
+          $(password).on('input', function(){
+            $('.contents3__entry__passwordResult').text();
+          });
+        });
       }
   });
-});
+}
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   var password  = '#js-password';
+//   var passcheck = '#js-passcheck';
+
+//   $(passcheck).change(function() {
+//     if ($(this).prop('checked')) {
+//       $(function(){
+//         $(password).on('input', function(){
+//           var input_text = $(this).val();
+//           $('.passwordResult').text(input_text);
+//         });
+//       });
+//     } else {
+//       $(function(){
+//         $(password).on('input', function(){
+//           $('.passwordResult').text();
+//         });
+//       });
+//     }
+//   });
+// });
+
+// 参考
+// パスワード表示時にマスキング有無を選択できるようにする方法 https://ajike.github.io/password-masking/
+// テキストボックスのリアルタイムな入力をイベントとして発火させる https://kinocolog.com/jquery_input/
