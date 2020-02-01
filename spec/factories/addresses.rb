8dc4676bb22692cc0780bf8619407db37c72b854
shuya-tamaru <tamaru1013@gmail.com>
2020-02-01 18:58:49 +0900

    addressモデルの単体テスト

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  
+  factory :address do
+    zip_code {"1111111"}
+    prefecture {"北海道"}
+    city       {"墨田区"}
+    address    {"八広"}
+    building   {"青葉ビル"}
+    phone_tel  {"0312345678"}
+  end
+
+end
