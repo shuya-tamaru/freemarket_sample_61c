8ae73ae47881eb847daa2b7b27d7583d96bdc345
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-02-02 13:10:42 +0900

    パスワード表示改変中

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -2,16 +2,14 @@ $(function() {
   var password  = '#js-password';
   var passcheck = '#js-passcheck';
 
-  changeInputtype(password, passcheck);
-});
-
-function changeInputtype(password, passcheck) {
-  $(passcheck).change(function() {
+    $(passcheck).change(function() {
       if ($(this).prop('checked')) {
         $(function(){
-          var input_text = $(password).val();
-          $('.contents3__entry__passwordResult').show();
-          $('.contents3__entry__passwordResult').text(input_text);
+          $(password).on('input', function(){
+            var input_text = $(password).val();
+            $('.contents3__entry__passwordResult').text(input_text);
+            $('.contents3__entry__passwordResult').show();
+          })
         });
       } else {
         $(function(){
@@ -19,38 +17,52 @@ function changeInputtype(password, passcheck) {
         });
       }
   });
-}
-
-
-
-
-
-
-
-
-
+});
 
 // $(function() {
 //   var password  = '#js-password';
 //   var passcheck = '#js-passcheck';
 
-//   $(passcheck).change(function() {
-//     if ($(this).prop('checked')) {
-//       $(function(){
-//         $(password).on('input', function(){
-//           var input_text = $(this).val();
-//           $('.passwordResult').text(input_text);
-//         });
-//       });
-//     } else {
-//       $(function(){
-//         $(password).on('input', function(){
-//           $('.passwordResult').text();
-//         });
-//       });
-//     }
-//   });
+//   changeInputtype(password, passcheck);
 // });
+
+// function changeInputtype(password, passcheck) {
+//   $(passcheck).change(function() {
+//       if ($(this).prop('checked')) {
+//         $(function(){
+//           var input_text = $(password).val();
+//           $('.contents3__entry__passwordResult').text(input_text);
+//           $('.contents3__entry__passwordResult').show();
+//         });
+//       } else {
+//         $(function(){
+//           $('.contents3__entry__passwordResult').hide();
+//         });
+//       }
+//   });
+// }
+
+$(function() {
+  var password  = '#js-password';
+  var passcheck = '#js-passcheck';
+
+  $(passcheck).change(function() {
+    if ($(this).prop('checked')) {
+      $(function(){
+        $(password).on('input', function(){
+          var input_text = $(this).val();
+          $('.passwordResult').text(input_text);
+        });
+      });
+    } else {
+      $(function(){
+        $(password).on('input', function(){
+          $('.passwordResult').text();
+        });
+      });
+    }
+  });
+});
 
 // 参考
 // パスワード表示時にマスキング有無を選択できるようにする方法 https://ajike.github.io/password-masking/
