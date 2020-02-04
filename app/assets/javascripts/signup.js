708625d0c42e23778dfeac68b66949d1093434c7
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-02-05 02:28:02 +0900

    パスワード表示機能修正、フッター上部マージン修正

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -2,69 +2,13 @@ $(function() {
   var password  = '#js-password';
   var passcheck = '#js-passcheck';
 
-    $(passcheck).change(function() {
-      if ($(this).prop('checked')) {
-        $(function(){
-          $(password).on('input', function(){
-            var input_text = $(password).val();
-            $('.contents3__entry__passwordResult').show();
-            $('.contents3__entry__passwordResult').text(input_text);
-          })
-        });
-      } else {
-        $(function(){
-          $('.contents3__entry__passwordResult').hide();
-        });
-      }
+  $(password).on("keyup",function() {
+    $(".contents3__entry__passwordResult__string").text($(password).val());
+  });
+  $(passcheck).on('change', function () {
+    $('.contents3__entry__passwordResult__string').toggle();
   });
 });
-
-
-
-// $(function() {
-//   var password  = '#js-password';
-//   var passcheck = '#js-passcheck';
-
-//   changeInputtype(password, passcheck);
-// });
-
-// function changeInputtype(password, passcheck) {
-//   $(passcheck).change(function() {
-//       if ($(this).prop('checked')) {
-//         $(function(){
-//           var input_text = $(password).val();
-//           $('.contents3__entry__passwordResult').text(input_text);
-//           $('.contents3__entry__passwordResult').show();
-//         });
-//       } else {
-//         $(function(){
-//           $('.contents3__entry__passwordResult').hide();
-//         });
-//       }
-//   });
-// }
-
-// $(function() {
-//   var password  = '#js-password';
-//   var passcheck = '#js-passcheck';
-
-//   $(passcheck).change(function() {
-//     if ($(this).prop('checked')) {
-//       $(function(){
-//         $(password).on('input', function(){
-//           var input_text = $(this).val();
-//           $('.passwordResult').text(input_text);
-//         });
-//       });
-//     } else {
-//       $(function(){
-//         $(password).on('input', function(){
-//           $('.passwordResult').text();
-//         });
-//       });
-//     }
-//   });
-// });
 
 // 参考
 // パスワード表示時にマスキング有無を選択できるようにする方法 https://ajike.github.io/password-masking/
