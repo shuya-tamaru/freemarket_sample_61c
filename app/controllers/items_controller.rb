57551f1e0d98e1e1eff9571f0b13eba6eac1c01b
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-06 21:11:07 +0900

    コントローラーにcreateとitem_paramsを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,22 @@ class ItemsController < ApplicationController
     @products_c = ["レディース", "メンズ", "家電・スマホ・カメラ", "おもちゃ・ホビー・グッズ"]
     @products_b = ["シャネル","ルイヴィトン","シュプリーム","ナイキ"]
   end
+
+  def new
+
+  end
+
+  def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to :index
+  else
+    redirect_to :new
+  end
+
+  private
+
+  def item_params
+    params.require(:items).permit(:image, :name, :discription, :grand_chaild_category, :brand, :saler_user_id, :item_status, :shipping_charge, :shipping_way, :shipping_days, :price, :region)
+  end
 end
