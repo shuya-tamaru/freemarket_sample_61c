5f0664be93b5c4bbf05999c836ee1dd1f551760c
shuya-tamaru <tamaru1013@gmail.com>
2020-02-09 12:58:10 +0900

    newアクションのテスト

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 describe Users::RegistrationsController do
 
+  describe 'GET #new' do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      get :new
+    end
+    it "renders the :new template" do
+      expect(response).to render_template :new
+    end
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
+    end
+    it '@userに新しいユーザーを割り当てること' do
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+    # describe 'GET #create' d
+    # end
 end
