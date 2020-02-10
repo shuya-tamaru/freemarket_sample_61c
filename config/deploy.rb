e12e3ea507c7e96de01fe57cc566cf6fd2d791ea
fukumototakaaki <wwwdocomo.ne.jpwwwdocomo.ne.jp@gmail.com>
2020-02-10 21:24:04 +0900

    deploy修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'mercari'
+set :application, 'freemarket_sample_61c'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:fukumototakaaki/mercari.git'
+set :repo_url,  'git@github.com:fukumototakaaki/freemarket_sample_61c.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
