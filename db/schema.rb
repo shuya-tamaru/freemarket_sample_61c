b7286075843085ab13fcbfbc76517c4b4945f8ab
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-02-11 01:10:58 +0900

    カード有効期限年月モデル作成と入力フォームの修正、前コミットで作成した余計なマイグレーションファイルを削除。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_131957) do
+ActiveRecord::Schema.define(version: 2020_01_28_144921) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -60,13 +60,6 @@ ActiveRecord::Schema.define(version: 2020_02_10_131957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_items_on_name"
-  end
-
-  create_table "todofukens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
