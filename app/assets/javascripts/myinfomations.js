19d8b79403f3185ea452ca219a8bee6cb5f9f49b
fukumototakaaki <wwwdocomo.ne.jpwwwdocomo.ne.jp@gmail.com>
2020-02-11 22:20:36 +0900

    取引中、過去の取引のjQuery仮実装

--- app/assets/javascripts/myinfomations.js
+++ app/assets/javascripts/myinfomations.js
@@ -1,18 +1,17 @@
 $(function() {
- 
-  // ①タブをクリックしたら発動
   $('.myinfomations__tabs__label li').click(function() {
- 
-    // ②クリックされたタブの順番を変数に格納
     var index = $('.myinfomations__tabs__label li').index(this);
- 
-    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
     $('.myinfomations__tabs__label li').removeClass('active');
- 
-    // ④クリックされたタブにクリック済みデザインを適用する
     $(this).addClass('active');
- 
-    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
     $('.myinfomations__tabs__contents ul').removeClass('view').eq(index).addClass('view');
+  });
+});
+
+$(function() {
+  $('.myinfomations__trade__label li').click(function() {
+    var index = $('.myinfomations__trade__label li').index(this);
+    $('.myinfomations__trade__label li').removeClass('active');
+    $(this).addClass('active');
+    $('.myinfomations__trade__contents ul').removeClass('view').eq(index).addClass('view');
   });
 });
