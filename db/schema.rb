ddec972b4f282d1926f164309ca7815f4f86d949
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-02-14 23:29:57 +0900

    no_password
    パスワードなしでのログイン

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_144921) do
+ActiveRecord::Schema.define(version: 2020_02_14_082641) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 2020_01_28_144921) do
     t.index ["name"], name: "index_items_on_name"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -86,4 +95,5 @@ ActiveRecord::Schema.define(version: 2020_01_28_144921) do
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
   add_foreign_key "cellphones", "users"
+  add_foreign_key "sns_credentials", "users"
 end
