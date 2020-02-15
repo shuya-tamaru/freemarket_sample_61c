faefa322c28ee4c9953c779a8990ad0ce04bc4bd
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-15 16:50:23 +0900

    newのビュー変更とfactoryの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,18 +7,21 @@ class ProductsController < ApplicationController
 
   def create
   @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path, notice: "商品を投稿しました"
-    else
-      redirect_to new_item_path, notice: "error"
-    end
+  @item.save
+  redirect_to root_path
+
+    # if @item.save
+    #   redirect_to root_path, notice: "商品を投稿しました"
+    # else
+    #   redirect_to new_product_path, notice: "error"
+    # end
   end
 
   private
 
   def item_params
+    params.require(:item).permit(:image, :category_id, :name, :discription, :brand_id, :item_status, :shipping_charge, :shipping_way, :sipping_days, :price, :region).merge(saler_user_id: current_user.id)
     binding.pry
-    params.require(:item).permit(:image, :category, :name, :discription, :brand, :saler_user_id, :item_status, :shipping_charge, :shipping_way, :sipping_days, :price, :region)
   end
   
 end
