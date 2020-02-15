59d86426efea8ffb4649b56c5309b4c45a2bcb4f
shuya-tamaru <tamaru1013@gmail.com>
2020-02-15 21:04:21 +0900

    半田さんのコード

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,15 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @item =Item.new
+    @item.images.build
   end
 
   def create
-  item = Item.new(item_params)
-  item.save!
-  redirect_to root_path
+    item = Item.new(item_params)
+    item.save!
+    redirect_to root_path
+
 
     # if @item.save
     #   redirect_to root_path, notice: "商品を投稿しました"
@@ -20,7 +23,7 @@ class ProductsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:fee_side, :category_id, :name, :discription, :brand_id, :item_status, :shipping_charge, :shipping_way, :sipping_days, :price, :region, images_attributes: [:image]).merge(saler_user_id: current_user.id.to_i, transaction_status: 1)
+    params.require(:item).permit(:fee_side, :category_id, :name, :discription, :brand_id, :item_status, :shipping_charge, :shipping_way, :sipping_days, :price, :region, images_attributes:[:image, :id]).merge(saler_user_id: current_user.id.to_i, transaction_status: 1) 
   end
   
 end
