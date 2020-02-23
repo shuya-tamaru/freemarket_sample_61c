1c575a69105f91686d2832faa2e385a48ff1718c
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-23 21:23:58 +0900

    プレビュー周辺の編集

--- app/assets/javascripts/product-image.js
+++ app/assets/javascripts/product-image.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="contents__item__upload__image__btn__area__fieldjs-file_group">
                     <input class="js-file" type="file"
@@ -8,15 +7,15 @@ $(document).on('turbolinks:load', ()=> {
                   </div>`;
     return html;
   }
-  // プレビュー用のimgタグを生成する関数
+
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html =
+    `<img class="contents__item__upload__image__preview" img data-index="${index}" img src="${url}" width="100px" height="100px"></img>`;
     return html;
   }
 
-  // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 既に使われているindexを除外
+
   lastIndex = $('.contents__item__upload__image__btn__area__field:last').data('index');
   fileIndex.splice(0, lastIndex);
 
@@ -24,19 +23,18 @@ $(document).on('turbolinks:load', ()=> {
 
   $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
-    // ファイルのブラウザ上でのURLを取得する
+
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
 
-    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('image', blobUrl);
-    } else {  // 新規画像追加の処理
+    } else {
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      // fileIndexの先頭の数字を使ってinputを作る
+
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
-      // 末尾の数に1足した数を追加する
+
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
   });
