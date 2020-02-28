fcbeca15b1c051d22e8acdcb0bddada6509bb1a8
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-02-28 12:14:16 +0900

    merge_master

--- db/schema.rb
+++ db/schema.rb
@@ -95,15 +95,6 @@ ActiveRecord::Schema.define(version: 2020_02_15_085837) do
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
