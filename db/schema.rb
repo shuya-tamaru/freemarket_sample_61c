3a876c5f2f778e0a9f201f78c8d80cc21aace082
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-02-29 18:33:30 +0900

    cardモデルにてpay.jpから返ってくる値を入れるカラムをテーブルに追加
    cardsテーブルにカラム追加するためのマイグレーションファイルを作成。
    
    # 新たに追加した2行について
    セキュリティーの観点からカード情報そのものをDBに保存することは禁止されている(改正割賦販売法)。
    そのため、pay.jpに保管されている情報を顧客IDやカードIDで呼び出すことで情報取得や支払いなどに対応する。
    ここで追加した2行は、customer_idは顧客ID情報(pay.jpから返ってくるデータ)、card_idはカードID情報(pay.jpから返ってくるデータ)を意味しており、16桁の番号、有効期限、セキュリティコードを渡すとpay.jpから返ってくる。
    pay.jp関係の実装が上手くいったことを確認した後、number, validated_date_yewr, validated_date_month, security_numberは削除する。
    # 注意：card_idはトークンではない！
    # 参考：http://payjp-announce.hatenablog.com/entry/2017/11/10/182738

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_100018) do
+ActiveRecord::Schema.define(version: 2020_02_29_092718) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2020_02_18_100018) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id"
+    t.string "card_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
