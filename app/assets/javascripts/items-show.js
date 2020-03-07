1bb24ed0fcce1d98a071742529f67d3467344830
fukumototakaaki <wwwdocomo.ne.jpwwwdocomo.ne.jp@gmail.com>
2020-03-07 17:17:13 +0900

    slick（jquery）導入準備

--- app/assets/javascripts/items-show.js
+++ app/assets/javascripts/items-show.js
@@ -1,5 +1,25 @@
-$(function() {
-  $('.item-box__details__image__list li img').hover(function(){
-    $('.item-box__details__image__top img').attr('src', $(this).attr('src'));
+$(document).on('turbolinks:load', function(){
+  var slider = ".item-box__details__image__top li img";
+  var thumbnailItem = ".item-box__details__image__list li img";
+
+  $(thumbnailItem).each(function(){
+    var index = $(thumbnailItem).index(this);
+    $(this).attr("data-index",index);
+   });
+
+   $(slider).slick({
+    arrows: false,
+    infinite: false
   });
+
+  $(thumbnailItem).on('click',function(){
+    var index = $(this).attr("data-index");
+    $(slider).slick("slickGoTo",index);
+  });
+
+
+  // $(function() {
+  //   $('.item-box__details__image__list li img').mouseover(function(){
+  //     $('.item-box__details__image__top img').attr('src', $(this).attr('src'));
+  //   });
 });
