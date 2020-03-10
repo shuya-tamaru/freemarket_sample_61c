101ab735d6ffcf1d1ba07a898bb6e14513836130
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-03-10 19:05:51 +0900

    大枠の作成
    ・payjpレスポンスのモック
    ・orders_controllerのテストの流れを作成
    ・cardsのテスト用のnumberをテストカード用のものに変更
    ・device_macrosでログイン・ログアウトメソッドを定義

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   
   factory :card do
-    number              {Faker::Number.number(digits: 16)}
+    number              {'4242424242424242'}
     validated_date_year  {Faker::Number.between(from: 20, to: 40)}
     validated_date_month {Faker::Number.between(from: 1, to: 12)}
     security_number     {Faker::Number.number(digits: 3)}
---
+++ spec/support/devise_macros.rb
@@ -0,0 +1,13 @@
+module DeviseMacros
+  def login_user(user)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  def logout_user(user)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_out user
+  end
+end
+
+#参考：https://qiita.com/kentosasa/items/a0500f2f21b33b29beb6
