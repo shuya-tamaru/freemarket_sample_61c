16d556461dc4c597f3684e348df5db603005c796
shuya-tamaru <tamaru1013@gmail.com>
2020-03-13 21:48:34 +0900

    編集機能フロント修正以外ほぼ完了。

--- app/assets/javascripts/product-image.js
+++ app/assets/javascripts/product-image.js
@@ -18,16 +18,6 @@ $(window).on('load', ()=> {
     return html2;
   }
 
-  // const editFileField = (num)=> {
-  //   const html = `  
-  //                   <input class="js__input"
-  //                   id="item_images_attributes_${num}_image" type="file"
-  //                   name="item[images_attributes][${num}][image]">
-  //                 `;
-  //   return html;
-  // }
-
-
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
   lastIndex = $('.contents__item__upload__image__btn__area__field:last').data('index');
@@ -44,32 +34,117 @@ $(window).on('load', ()=> {
     }
   });
 
-  $('.hidden-destroy').hide();
-
-  $('#image-box').on('change', function(e) {
-    const targetIndex = $(this).parent().data('index');
-    const num = $(".image-box__images__box").length;
-
-    const file = e.target.files[0];
-    const blobUrl = window.URL.createObjectURL(file);
-    if (num != 9){
-      $('.image-box__images').before(buildImg(num, blobUrl));
-
-      $('#image-box').prepend(buildFileField(num+1));
-
-      $('.text').remove();
-      } else {
+   $('.hidden-destroy').hide();
+  var  nowurl   = location.href;
+  if(nowurl.match(/new/)){
+	
+    $('#image-box').on('change', function(e) {
+      const targetIndex = $(this).parent().data('index');
+      const num = $(".image-box__images__box").length;
+      const file = e.target.files[0];
+      const blobUrl = window.URL.createObjectURL(file);
+      if (num != 9){
         $('.image-box__images').before(buildImg(num, blobUrl));
-      }
-  });
 
-  $('#image-box').on('click', '.img_delete', function() {
+        $('#image-box').prepend(buildFileField(num+1));
+
+        $('.text').remove();
+        } else {
+          $('.image-box__images').before(buildImg(num, blobUrl));
+        }
+
+    });
+  }
+
+
+  if(nowurl.match(/edit/)){
+    
+    $('.label-box').on('change', function(e) {
+      const targetIndex = $(this).parent().data('index');
+      const num = $(".image-box__images__box").length;
+      const file = e.target.files[0];
+      const blobUrl = window.URL.createObjectURL(file);
+      console.log("hello")
+
+      //labelボックスのidとforを更新
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      $('.label-box').attr({id: `label-box--${Number(id)+1}`,for: `item_images_attributes_${Number(id)+1}_image`});
+
+      if (num != 9){
+        $('.image-box__images').append(buildImg(num, blobUrl));
+
+        $('.image-box__images').after(buildFileField(num+1));
+
+        $('.text').remove();
+        } else {
+          $('.image-box__images').append(buildImg(num, blobUrl));
+        }
+    });
+  }
+
+  $('#image-box').on('click', '.image__delete', function() {
     const targetIndex = $(this).data('index')
+    console.log("111")
+
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).remove();
+    $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
   });
 
 
 });
+
+// $(window).on('load', ()=> {
+
+//     const buildFileField = (num)=> {
+//     const html = `
+//                   <input class="image-box__images__input"
+//                   id="item_images_attributes_${num}_image" type="file"
+//                   name="item[images_attributes][${num}][image]">
+//                   `;
+//     return html;
+//   }
+
+//     const buildImg = (index, url)=> {
+//     const html2 = `
+//     <div class= "image-box__images__box">
+//       <img class= "image-box__images__box__image${index}" img src="${url}" width="110px" height="120px"></img>
+//       <div class= "image-box__images__box__delete" width="110px" height="20px">削除</div>
+//     </div>
+//       `;
+//     return html2;
+//   }
+
+//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+//     $('.image-box__images__input').on('change', function(e) {
+//       const targetIndex = $(this).parent().data('index');
+//       const num = $(".image-box__images__box").length;
+//       const file = e.target.files[0];
+//       const blobUrl = window.URL.createObjectURL(file);
+//       console.log("hello")
+//       if (num != 9){
+//         $('.image-box__images').append(buildImg(num, blobUrl));
+
+//         $('.image-box__images').after(buildFileField(num+1));
+
+//         $('.text').remove();
+//         } else {
+//           $('.image-box__images').append(buildImg(num, blobUrl));
+//         }
+//     });
+
+//   $('#image-box').on('click', '.image-box__images__box__delete', function() {
+//     console.log("bbb")
+
+//     $(this).parent().remove();
+//     // 画像入力欄が0個にならないようにしておく
+//     if ($('.image-box__images__input').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+//   });
+
+// });
+
+
+
+
