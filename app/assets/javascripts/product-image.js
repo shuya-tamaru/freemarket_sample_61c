8c2698e284f07e2b80a10eee7ca4b14731d216d6
shuya-tamaru <tamaru1013@gmail.com>
2020-03-14 17:29:59 +0900

    途中経過

--- app/assets/javascripts/product-image.js
+++ app/assets/javascripts/product-image.js
@@ -1,5 +1,7 @@
 $(window).on('load', ()=> {
+  // const buildFileField = (num)=> {
   const buildFileField = (num)=> {
+
     const html = `
                   <input class="image-box__images__input"
                   id="item_images_attributes_${num}_image" type="file"
@@ -18,26 +20,41 @@ $(window).on('load', ()=> {
     return html2;
   }
 
+// //  edit用の要素
+//   const buildImg_edit = (index, url)=> {
+//     const html2 = `
+//     <div class= "image-box__images__box">
+//       <img class= "image-box__images__box__image${index}" img src="${url}" width="110px" height="120px"></img>
+//       <div class= "image-box__images__box__delete${index}" width="110px" height="20px">削除</div>
+//     </div>
+//       `;
+//     return html2;
+//   }
+
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
   lastIndex = $('.contents__item__upload__image__btn__area__field:last').data('index');
   fileIndex.splice(0, lastIndex);
 
-  $(document).on("click", ".image-box__images__box__delete", function(event) {
-    event.preventDefault();
-    const num = $(".image-box__images__box").length;
-    const deleteId = ("#item_images_attributes_"+num+"_image")
-    $(this).closest(".image-box__images__box").remove();
-    $(deleteId).remove();
-    if (num == 9){
-      $('#image-box').prepend(buildFileField(9))
-    }
-  });
 
-   $('.hidden-destroy').hide();
+  $('.hidden-destroy').hide();
   var  nowurl   = location.href;
+
   if(nowurl.match(/new/)){
-	
+
+    $(document).on("click", ".image-box__images__box__delete", function(event) {
+      event.preventDefault();
+      console.log(222)
+      const num = $(".image-box__images__box").length;
+      const deleteId = ("#item_images_attributes_"+num+"_image")
+      $(this).closest(".image-box__images__box").remove();
+      $(deleteId).remove();
+      if (num == 9){
+        $('#image-box').prepend(buildFileField(9))
+      }
+    });
+  // if(nowurl.match(/new/)){
+
     $('#image-box').on('change', function(e) {
       const targetIndex = $(this).parent().data('index');
       const num = $(".image-box__images__box").length;
@@ -52,7 +69,6 @@ $(window).on('load', ()=> {
         } else {
           $('.image-box__images').before(buildImg(num, blobUrl));
         }
-
     });
   }
 
@@ -66,85 +82,41 @@ $(window).on('load', ()=> {
       const blobUrl = window.URL.createObjectURL(file);
       console.log("hello")
 
-      //labelボックスのidとforを更新
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
       $('.label-box').attr({id: `label-box--${Number(id)+1}`,for: `item_images_attributes_${Number(id)+1}_image`});
 
       if (num != 9){
         $('.image-box__images').append(buildImg(num, blobUrl));
-
-        $('.image-box__images').after(buildFileField(num+1));
-
+        $('.image-box__images').after(buildFileField(Number(id)+1));
         $('.text').remove();
         } else {
           $('.image-box__images').append(buildImg(num, blobUrl));
         }
     });
+  
+    $('#image-box').on('click', '.image-box__images__box__delete', function(e) {
+    // $('#image-box').on('click', '.image__delete', function(e) {
+
+      event.preventDefault();
+      const num = $(".image-box__images__box").length;
+      const targetIndex = $(this).data('index')
+
+      console.log(targetIndex)
+      console.log("111")
+      
+      const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+
+      // const deleteId = ("#item_images_attributes_"+num+"_image")
+      // $(deleteId).remove();
+
+      if (hiddenCheck) hiddenCheck.prop('checked', true);
+        $(this).parent().remove();
+        $(`img[data-index="${targetIndex}"]`).remove();
+      // if (num == 10){
+      //   console.log("33333")
+      //   $('.image-box__images').after(buildFileField(20));
+      // }
+    });
   }
-
-  $('#image-box').on('click', '.image__delete', function() {
-    const targetIndex = $(this).data('index')
-    console.log("111")
-
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-  });
-
-
 });
-
-// $(window).on('load', ()=> {
-
-//     const buildFileField = (num)=> {
-//     const html = `
-//                   <input class="image-box__images__input"
-//                   id="item_images_attributes_${num}_image" type="file"
-//                   name="item[images_attributes][${num}][image]">
-//                   `;
-//     return html;
-//   }
-
-//     const buildImg = (index, url)=> {
-//     const html2 = `
-//     <div class= "image-box__images__box">
-//       <img class= "image-box__images__box__image${index}" img src="${url}" width="110px" height="120px"></img>
-//       <div class= "image-box__images__box__delete" width="110px" height="20px">削除</div>
-//     </div>
-//       `;
-//     return html2;
-//   }
-
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//     $('.image-box__images__input').on('change', function(e) {
-//       const targetIndex = $(this).parent().data('index');
-//       const num = $(".image-box__images__box").length;
-//       const file = e.target.files[0];
-//       const blobUrl = window.URL.createObjectURL(file);
-//       console.log("hello")
-//       if (num != 9){
-//         $('.image-box__images').append(buildImg(num, blobUrl));
-
-//         $('.image-box__images').after(buildFileField(num+1));
-
-//         $('.text').remove();
-//         } else {
-//           $('.image-box__images').append(buildImg(num, blobUrl));
-//         }
-//     });
-
-//   $('#image-box').on('click', '.image-box__images__box__delete', function() {
-//     console.log("bbb")
-
-//     $(this).parent().remove();
-//     // 画像入力欄が0個にならないようにしておく
-//     if ($('.image-box__images__input').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-//   });
-
-// });
-
-
-
 
