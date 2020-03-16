e30e856347524fe6c97480fbd96b08cda760d144
Ynobusa <58387687+Ynobusa@users.noreply.github.com>
2020-03-16 22:30:16 +0900

    モック修正。変数が正しく定義されているかのテスト追記。

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,22 +5,39 @@ describe OrdersController do
   #create_list(<作成したいファクトリ名>, <作成するインスタンス数(購入者と出品者を用意するため。)>)
   let(:category) { create(:category) }
   let(:brand) { create(:brand) }
-  let(:image) { create(:image) }
   let(:item) { create(:item, saler_user_id: users.first.id, category_id: category.id, brand_id: brand.id) }
+  let(:image) { create(:image, item_id: item.id) }
   let(:card) { create(:card, user_id: users.first.id) }
-  let(:address) { create(:address) }
+  let(:address) { create(:address, user_id: users.first.id) }
 
   describe 'GET #new' do
     context 'when user login' do
       before do
-        login users.last
+        login users.first
         @item = item
         @images = @item.images
         @address = address
         get :new, params:{id: @item[:id]}
       end
       it "renders the :new template" do
+        allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer)
         expect(response).to render_template :new
+      end
+
+      it "- @itemという変数が正しく定義" do
+        expect(assigns(:item)).to eq(@item)
+      end
+
+      it "- @imagesという変数が正しく定義" do
+        expect(assigns(:image)).to eq(@images)
+      end
+
+      it "- @cardという変数が正しく定義" do
+        expect(assigns(:card)).to eq(@card)
+      end
+
+      it "- @addressという変数が正しく定義" do
+        expect(assigns(:address)).to eq(@address)
       end
     end
   end
