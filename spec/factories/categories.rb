520a722f1eff5180fe25fbee629f221d7735b02c
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-03-23 20:53:29 +0900

    categoriesのfactory

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :category do
-    
+    id {1}
+    name {"レディース"}
+    #このfactoryはitem_specで使用しているが、ancestryを記述すると更にCategoryのアソシエーションを読みに行ってエラーになるため記述しない
+    # ancestryをテストする場合、別の名前のfactoryを作ってください
   end
 end
