7d0160ecb7bdbfbbcc7e0cd05149d95f944c75e0
shuya-tamaru <tamaru1013@gmail.com>
2020-03-24 21:38:54 +0900

    ログイン出来ないテスト
    emailが空でログイン出来ない
    ❶new_user_session_path　にrenderされることで確認
    ❷renderされたページにログインという文字があることで確認

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -13,4 +13,13 @@ feature 'user_signin', type: :feature do
     expect(page).to have_content('マイページ')
   end
 
+  scenario "user doesn't login with invalid information" do
+    visit new_user_session_path
+    fill_in 'user_email', with: ""
+    fill_in 'user_password', with: user.password
+    click_button "ログイン"
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('ログイン')
+  end
+
 end
