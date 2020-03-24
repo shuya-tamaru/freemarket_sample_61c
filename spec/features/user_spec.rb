2cac079fdcac5021b2097f9c9c5cd8c31b977088
shuya-tamaru <tamaru1013@gmail.com>
2020-03-24 21:30:38 +0900

    ログインのテスト
      ❶ログイン画面でフォームを入力し、インデックスに飛ぶことでログイン出来てることを確認。
     ❷ヘッダーにマイページの文字が出てくることでもログインを確認

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
+feature 'user_signin', type: :feature do
+
+  let(:user) { FactoryBot.create(:user) }
+  
+  scenario "user successfully login" do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button "ログイン"
+    expect(current_path).to eq root_path
+    expect(page).to have_content('マイページ')
+  end
 
 end
