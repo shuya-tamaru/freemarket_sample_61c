281af98a4a8f214f95d240a0440786fdc9a59b24
shuya-tamaru <tamaru1013@gmail.com>
2020-03-24 21:53:29 +0900

    ログアウトのテスト

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -23,3 +23,26 @@ feature 'user_signin', type: :feature do
   end
 
 end
+
+feature 'user_signout', type: :feature do
+
+  let(:user) { FactoryBot.create(:user) }
+  
+  scenario "user successfully signout" do
+    #ログインする
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button "ログイン"
+    expect(current_path).to eq root_path
+    expect(page).to have_content('マイページ')
+
+    # ログアウトのテスト
+    visit myinfomations_path
+    visit user_path(user.id)
+    click_on(class: 'logout__button')
+    expect(current_path).to eq root_path
+    expect(page).to have_content('ログイン')
+  end
+
+end
